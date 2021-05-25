87e553c8b82584688b06a5fb6ad2464213a7acff
easonlchen <easonlchen@tencent.com>
2021-05-25 20:39:53 +0800

    添加图片上传

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button, Image } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 // import { AtButton } from 'taro-ui'
 
 import "taro-ui/dist/style/components/button.scss" // 按需引入
@@ -7,20 +8,59 @@ import './index.scss'
 
 export default class Index extends Component {
 
-  componentWillMount () { }
+  state = {
+    tempFilePaths: '',
+  }
 
-  componentDidMount () { }
+  componentWillMount() { }
 
-  componentWillUnmount () { }
+  componentDidMount() {
+    Taro.request({
+      url: 'https://fmu.tencentcloudapi.com/?Action=GetModelList', //仅为示例，并非真实的接口地址
+      data: {
+        Action: 'GetModelList',
+        Version: '2019-12-13',
+        Region: 'ap-shanghai',
+        Timestamp: '2019-12-13'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+      }
+    })
+  }
 
-  componentDidShow () { }
+  componentWillUnmount() { }
 
-  componentDidHide () { }
+  componentDidShow() { }
 
-  render () {
+  componentDidHide() { }
+
+  uploadImg = () => {
+    Taro.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths[0];
+        this.setState({ tempFilePaths });
+        console.log(tempFilePaths, 'tempFilePaths-tempFilePaths');
+      }
+    })
+  }
+
+  render() {
+    const { tempFilePaths } = this.state;
     return (
       <View className='index'>
-        <Text>试色</Text>
+        <Button onClick={this.uploadImg}>上传照片</Button>
+        <Image
+          className='image'
+          src={tempFilePaths}
+        />
       </View>
     )
   }
